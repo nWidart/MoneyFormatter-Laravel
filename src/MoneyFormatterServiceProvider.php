2e63a725bd0c1586b0c779564195ac8ba6251e51
Nicolas Widart <n.widart@gmail.com>
2015-03-22 20:47:48 +0100

    Prepare the Service Provider

---
+++ src/MoneyFormatterServiceProvider.php
@@ -0,0 +1,43 @@
+<?php namespace Nwidart\MoneyFormatterLaravel;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+use Nwidart\MoneyFormatter\MoneyFormatter;
+
+class MoneyFormatterServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->registerConfiguration();
+        $this->registerAlias();
+
+        $this->app->bindShared('Nwidart\MoneyFormatter\MoneyFormatter', function () {
+            return new MoneyFormatter(config('money-formatter.locale', 'en_UK'));
+        });
+    }
+
+    public function provides()
+    {
+        return ['money.formatter'];
+    }
+
+    /**
+     * Register the configuration file so Laravel can publish them
+     * Also merges the published config file with original
+     */
+    private function registerConfiguration()
+    {
+        $configPath = __DIR__ . '/../config/money-formatter.php';
+        $this->mergeConfigFrom($configPath, 'money-formatter');
+        $this->publishes([$configPath => config_path('money-formatter.php')]);
+    }
+
+    /**
+     * Register the MoneyFormatter laravel alias.
+     */
+    private function registerAlias()
+    {
+        $aliasLoader = AliasLoader::getInstance();
+        $aliasLoader->alias('MoneyFormatter', 'Nwidart\MoneyFormatterLaravel\MoneyFormatter');
+    }
+}
